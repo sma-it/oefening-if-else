57b6fe831e467c8a4dce965c4a460c2b34c93a63
tinahauq <40709994+tinahauq@users.noreply.github.com>
2018-07-08 10:20:53 +0200

    Test repo (with solution)

---
+++ oefening/Program.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace First
+{
+	class Program
+	{
+		/* Geef het grootste argument als resultaat van de functie. Ga ervan uit dat de argumenten verschillend zijn.
+		*/
+		public static int bepaalGrootste(int a, int b)
+		{
+            //schrijf hier je code
+            int grootste;
+            if(a>b)
+            {
+                grootste=a;
+            }
+            else
+            {
+                grootste=b;
+            }
+			return grootste;
+		}
+
+        /* Ga na of twee getallen gelijk zijn. Geef true (gelijk) of false (niet gelijk) als resultaat
+        */
+        public static bool isGelijk(int a, int b)
+        {
+            bool gelijk = false;
+            if(a==b)
+            {
+                gelijk = true;
+            }
+            return gelijk;
+        }
+
+            static void Main(string[] args)
+		{
+            Console.WriteLine("Naam oefening \t Verwacht resultaat \t Jouw resultaat");
+            Console.WriteLine("bepaalGrootste \t 3 \t\t\t " + bepaalGrootste(1, 3));
+            Console.WriteLine("bepaalGrootste \t 3 \t\t\t " + bepaalGrootste(3, 1));
+            Console.WriteLine("bepaalGrootste \t False \t\t\t " + isGelijk(5, 7));
+            Console.WriteLine("bepaalGrootste \t True \t\t\t " + isGelijk(5, 5));
+            Console.ReadKey();
+		}
+	}
+}
---
+++ oefening/Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnitLite;
+
+namespace Tests
+{
+	[TestFixture]
+	public class TestClass
+	{
+		// deze functie is nodig om de testen achteraf via de webinterface uit te voeren
+		public static int Run(string resultPath)
+		{
+			string[] args = { "--work=" + resultPath };
+			return new AutoRun().Execute(args);
+		}
+
+		[Test]
+		public void TestOefening1()
+		{
+			Assert.That(First.Program.bepaalGrootste(1, 3), Is.EqualTo(3), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+			Assert.That(First.Program.bepaalGrootste(3, 1), Is.EqualTo(3), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+            Assert.That(First.Program.isGelijk(5, 7), Is.EqualTo(false), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+            Assert.That(First.Program.isGelijk(5, 5), Is.EqualTo(true), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+        }
+	}
+}
