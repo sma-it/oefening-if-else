4475bb3dea7b5bbcc066a7b71bfdc666024adc94
tinahauq <40709994+tinahauq@users.noreply.github.com>
2018-07-09 10:53:31 +0200

    Added exercises and tests

--- oefening/Program.cs
+++ oefening/Program.cs
@@ -1,46 +1,129 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace First
+namespace If_else
 {
-    class Program
+    class Oef
     {
         /* Geef het grootste argument als resultaat van de functie. Ga ervan uit dat de argumenten verschillend zijn.
-		*/
-        public static int bepaalGrootste(int a, int b)
+        */
+        public static int BepaalGrootste(int a, int b)
         {
-            //schrijf hier je code
-            int grootste;
-            if (a > b)
-            {
-                grootste = a;
-            }
-            else
-            {
-                grootste = b;
-            }
-            return grootste;
+            // schrijf hier je code
+            return 0;
         }
 
-        /* Ga na of twee getallen gelijk zijn. Geef true (gelijk) of false (niet gelijk) als resultaat
+        /* Ga na of twee getallen gelijk zijn. Geef true (gelijk) of false (niet gelijk) als functieresultaat.
         */
-        public static bool isGelijk(int a, int b)
+        public static bool IsGelijk(int a, int b)
         {
-            bool gelijk = false;
-            if (a == b)
-            {
-                gelijk = true;
-            }
-            return gelijk;
+            // schrijf hier je code
+            return true;
+        }
+
+        /* Test of de drie doorgegeven hoeken de hoeken van een driehoek kunnen zijn. Geef als functieresultaat true (het zijn hoeken
+         * van een driehoek) of false (het zijn niet de hoeken van een driehoek).
+         */
+        public static bool HoekenDriehoek(int a, int b, int c)
+        {
+            // schrijf hier je code
+            return true;
+        }
+
+        /* Test of het argument een even getal bevat. Geef true (even getal) of false (oneven getal) als functieresultaat.
+        */
+        public static bool IsEven(int getal)
+        {
+            // schrijf hier je code
+            return true;
+        }
+
+        /* Test of het eeste getal deelbaar is door het tweede. Geef 'Deelbaar' of 'Niet deelbaar' als functieresultaat.
+         */
+        public static string IsDeelbaar(int getal1, int getal2)
+        {
+            // schrijf hier je code
+            return " ";
+        }
+
+        /* De x en y-coördinaat van een punt worden als argument doorgegeven aan de functie. Geef als functieresultaat het
+         * kwadrant waarin het punt gelegen is.
+         * Toon de volledige naam van het kwadrant, niet enkel het nummer: 'Kwadrant 1', 'Kwadrant 2', ...
+         */
+        public static string BepaalKwadrant(int x, int y)
+        {
+            // schrijf hier je code
+            return " ";
+        }
+
+        /* De lengte van de drie zijden van een driehoek worden als argument doorgegeven aan de functie. De functie geeft het
+         * type driehoek als resultaat ('Gelijkzijdig', 'Gelijkbenig', 'Ongelijkzijdig').
+         */
+        public static string BepaalType(float a, float b, float c)
+        {
+            // schrijf hier je code
+            return " ";
+        }
+
+        /* Ga na of het karakter een kleine letter of een hoofdletter is. Als er geen letter als karakter doorgegeven wordt aan de
+         * functie geef je een gepaste melding als functieresultaat.
+         * Tip: baseer je op de ASCII-tabel.
+         * Als functieresultaat geef je 'Hoofdletter', 'Kleine letter' of 'Geen letter'.
+         */
+        public static string TestLetter(char c)
+        {
+            // schrijf hier je code
+            return " ";
+        }
+
+        /* Ga na of het opgegeven jaartal een schrikkeljaar is. Als functieresultaat voorzie je 'Schrikkeljaar' en 'Geen schrikkeljaar'.
+         * Een jaar met jaartal deelbaar door 4 is een schrikkeljaar. Andere jaren zijn gewone jaren. 
+         * Maar omwille van de kalenderhervorming zijn ook de jaren deelbaar door 100 gewone jaren (ook al zijn ze deelbaar door 4),
+         * tenzij ze deelbaar zijn door 400. Dan zijn ze schrikkeljaren.
+         */
+        public static string TestSchrikkel(int jaartal)
+        {
+            // schrijf hier je code
+            return " ";
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Naam oefening \t Verwacht resultaat \t Jouw resultaat");
-            Console.WriteLine("bepaalGrootste \t 3 \t\t\t " + bepaalGrootste(1, 3));
-            Console.WriteLine("bepaalGrootste \t 3 \t\t\t " + bepaalGrootste(3, 1));
-            Console.WriteLine("bepaalGrootste \t False \t\t\t " + isGelijk(5, 7));
-            Console.WriteLine("bepaalGrootste \t True \t\t\t " + isGelijk(5, 5));
+            Console.WriteLine("BepaalGrootste \t 3 \t\t\t " + BepaalGrootste(1, 3));
+            Console.WriteLine("BepaalGrootste \t 3 \t\t\t " + BepaalGrootste(3, 1));
+            Console.WriteLine("IsGelijk \t False \t\t\t " + IsGelijk(5, 7));
+            Console.WriteLine("IsGelijk \t True \t\t\t " + IsGelijk(5, 5));
+            Console.WriteLine("HoekenDriehoek \t False \t\t\t " + HoekenDriehoek(50, 15, 20));
+            Console.WriteLine("HoekenDriehoek \t True \t\t\t " + HoekenDriehoek(90, 45, 45));
+            Console.WriteLine("IsEven \t\t False \t\t\t " + IsEven(5));
+            Console.WriteLine("IsEven \t\t True \t\t\t " + IsEven(4));
+            Console.WriteLine("IsDeelbaar \t Niet deelbaar \t\t " + IsDeelbaar(10, 4));
+            Console.WriteLine("IsDeelbaar \t Deelbaar \t\t " + IsDeelbaar(10, 5));
+            Console.WriteLine("BepaalKwadrant \t Kwadrant 1 \t\t " + BepaalKwadrant(2, 5));
+            Console.WriteLine("BepaalKwadrant \t Kwadrant 2\t\t " + BepaalKwadrant(-2, 5));
+            Console.WriteLine("BepaalKwadrant \t Kwadrant 3\t\t " + BepaalKwadrant(-3, -5));
+            Console.WriteLine("BepaalKwadrant \t Kwadrant 4\t\t " + BepaalKwadrant(3, -5));
+            Console.WriteLine("TestLetter \t Kleine letter\t\t " + TestLetter('d'));
+            Console.WriteLine("TestLetter \t Hoofdleter\t\t " + TestLetter('C'));
+            Console.WriteLine("TestLetter \t Geen letter\t\t " + TestLetter('%'));
+            Console.WriteLine("TestSchrikkel \t Geen schrikkeljaar\t " + TestSchrikkel(1000));
+            Console.WriteLine("TestSchrikkel \t Schrikkeljaar\t\t " + TestSchrikkel(1600));
+            Console.WriteLine("TestSchrikkel \t Geen schrikkeljaar\t " + TestSchrikkel(1950));
+            Console.WriteLine("TestSchrikkel \t Schrikkeljaar\t\t " + TestSchrikkel(2004));
+            Console.WriteLine("BepaalType \t Gelijkzijdig\t\t " + BepaalType(2.5F, 2.5F, 2.5F));
+            Console.WriteLine("BepaalType \t Gelijkbenig\t\t " + BepaalType(2.5F, 2.5F, 3.9F));
+            Console.WriteLine("BepaalType \t Gelijkbenig\t\t " + BepaalType(2.5F, 3.9F, 2.5F));
+            Console.WriteLine("BepaalType \t Gelijkbenig\t\t " + BepaalType(3.9F, 2.5F, 2.5F));
+            Console.WriteLine("BepaalType \t Ongelijkzijdig\t\t " + BepaalType(1.2F, 3.8F, 2.7F));
+
+
             Console.ReadKey();
         }
     }
 }
+
+
--- oefening/Test.cs
+++ oefening/Test.cs
@@ -3,23 +3,46 @@ using NUnitLite;
 
 namespace Tests
 {
-	[TestFixture]
-	public class TestClass
-	{
-		// deze functie is nodig om de testen achteraf via de webinterface uit te voeren
-		public static int Run(string resultPath)
-		{
-			string[] args = { "--work=" + resultPath };
-			return new AutoRun().Execute(args);
-		}
-
-		[Test]
-		public void TestOefening1()
-		{
-			Assert.That(First.Program.bepaalGrootste(1, 3), Is.EqualTo(3), "Dit wordt door de webinterface getoond wanneer de test faalt.");
-			Assert.That(First.Program.bepaalGrootste(3, 1), Is.EqualTo(3), "Dit wordt door de webinterface getoond wanneer de test faalt.");
-            Assert.That(First.Program.isGelijk(5, 7), Is.EqualTo(false), "Dit wordt door de webinterface getoond wanneer de test faalt.");
-            Assert.That(First.Program.isGelijk(5, 5), Is.EqualTo(true), "Dit wordt door de webinterface getoond wanneer de test faalt.");
+    [TestFixture]
+    public class TestClass
+    {
+        // deze functie is nodig om de testen achteraf via de webinterface uit te voeren
+        public static int Run(string resultPath)
+        {
+            string[] args = { "--work=" + resultPath };
+            return new AutoRun().Execute(args);
         }
-	}
+
+        [Test]
+        public void TestOefening1()
+        {
+            Assert.That(If_else.Oef.BepaalGrootste(2, 4), Is.EqualTo(4));
+            Assert.That(If_else.Oef.BepaalGrootste(7, 9), Is.EqualTo(9));
+            Assert.That(If_else.Oef.IsGelijk(5, 15), Is.EqualTo(false));
+            Assert.That(If_else.Oef.IsGelijk(25, 25), Is.EqualTo(true));
+            Assert.That(If_else.Oef.HoekenDriehoek(100, 25, 25), Is.EqualTo(false));
+            Assert.That(If_else.Oef.HoekenDriehoek(100, 40, 40), Is.EqualTo(true));
+            Assert.That(If_else.Oef.IsEven(25), Is.EqualTo(false));
+            Assert.That(If_else.Oef.IsEven(24), Is.EqualTo(true));
+            Assert.That(If_else.Oef.IsDeelbaar(125, 10), Is.EqualTo("Niet deelbaar"));
+            Assert.That(If_else.Oef.IsDeelbaar(12, 4), Is.EqualTo("Deelbaar"));
+            Assert.That(If_else.Oef.BepaalKwadrant(12, 15), Is.EqualTo("Kwadrant 1"));
+            Assert.That(If_else.Oef.BepaalKwadrant(-12, 15), Is.EqualTo("Kwadrant 2"));
+            Assert.That(If_else.Oef.BepaalKwadrant(-13, -15), Is.EqualTo("Kwadrant 3"));
+            Assert.That(If_else.Oef.BepaalKwadrant(13, -15), Is.EqualTo("Kwadrant 4"));
+            Assert.That(If_else.Oef.TestLetter('y'), Is.EqualTo("Kleine letter"));
+            Assert.That(If_else.Oef.TestLetter('Y'), Is.EqualTo("Hoofdletter"));
+            Assert.That(If_else.Oef.TestLetter('!'), Is.EqualTo("Geen letter"));
+            Assert.That(If_else.Oef.TestSchrikkel(3000), Is.EqualTo("Geen schrikkeljaar"));
+            Assert.That(If_else.Oef.TestSchrikkel(2400), Is.EqualTo("Schrikkeljaar"));
+            Assert.That(If_else.Oef.TestSchrikkel(1850), Is.EqualTo("Geen schrikkeljaar"));
+            Assert.That(If_else.Oef.TestSchrikkel(2008), Is.EqualTo("Schrikkeljaar"));
+            Assert.That(If_else.Oef.BepaalType(12.5F, 12.5F, 12.5F), Is.EqualTo("Gelijkzijdig"));
+            Assert.That(If_else.Oef.BepaalType(12.5F, 12.5F, 13.9F), Is.EqualTo("Gelijkbenig"));
+            Assert.That(If_else.Oef.BepaalType(12.5F, 13.9F, 12.5F), Is.EqualTo("Gelijkbenig"));
+            Assert.That(If_else.Oef.BepaalType(13.9F, 12.5F, 12.5F), Is.EqualTo("Gelijkbenig"));
+            Assert.That(If_else.Oef.BepaalType(11.2F, 13.8F, 12.7F), Is.EqualTo("Ongelijkzijdig"));
+        }
+    }
+
 }
